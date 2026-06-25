f6dfb556024831bae5decff81d2d724c3d35170d
mgsmariana123@gmail.com <mgsmariana123@gmail.com>
2026-06-25 16:47:42 -0300

    chore: initialize project structure and gitignore

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'API Investigation Lab funcionando 🚀'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
